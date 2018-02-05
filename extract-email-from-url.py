978d417b062659276bdf785a8a4640a75ad3339e
Vikram Karnaker <vkarnaker@gmail.com>
2018-02-05 18:37:29 -0500

    Code under development: Test code to find links to other pages and add them to the url queue

--- extract-email-from-url.py
+++ extract-email-from-url.py
@@ -32,6 +32,7 @@ while len(new_urls):
     parts = urlsplit(url)
     base_url = "{0.scheme}://{0.netloc}".format(parts)
     path = url[:url.rfind('/')+1] if '/' in parts.path else url
+    print(path)
 
     # get url's content
     print("Processing %s" % url)
@@ -44,6 +45,22 @@ while len(new_urls):
     # extract all email addresses and add them into the resulting set
     new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
     emails.update(new_emails)
+
+    # create a beutiful soup for the html document
+    soup = BeautifulSoup(response.text)
+
+    # find and process all the anchors in the document
+    for anchor in soup.find_all("a"):
+        # extract link url from the anchor
+        link = anchor.attrs["href"] if "href" in anchor.attrs else ''
+        # resolve relative links
+        if link.startswith('/'):
+            link = base_url + link
+        elif not link.startswith('http'):
+            link = path + link
+        # add the new url to the queue if it was not enqueued nor processed yet
+        if not link in new_urls and not link in processed_urls:
+            new_urls.append(link)
 
 # Write emails to timestamped CSV
 now = datetime.datetime.now()
