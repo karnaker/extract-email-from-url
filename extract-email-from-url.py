7c810283711f6ff440a5de5e4000442987a9e481
Vikram Karnaker <vkarnaker@gmail.com>
2018-02-06 16:00:55 -0500

    Step 2: Extract URLs from the set of URLs input by the user Program incomplete but headed in right direction

--- extract-email-from-url.py
+++ extract-email-from-url.py
@@ -9,10 +9,10 @@ import csv
 
 # user is prompted for a queue of urls to be crawled
 user_input_yes_no = "y"
-new_urls = deque([])
+user_urls = deque([])
 while user_input_yes_no == "y":
     user_input_url = input("Enter URL here: ")
-    new_urls.append(user_input_url)
+    user_urls.append(user_input_url)
     user_input_yes_no = input("Enter another URL ([y]/n)? ")
     while user_input_yes_no != "y" and user_input_yes_no != "n":
         print("Enter y or n")
@@ -21,16 +21,38 @@ while user_input_yes_no == "y":
 # a base url path to contain the crawl to
 user_input_contain_crawl_to_base_path = input("Enter path to contain the crawl to: ")
 
-# a set of urls that we have already crawled
+# a set of urls that we have already crawled for urls
+processed_user_urls = set()
+
+# a set of urls with emails that we will crawl
+new_urls = deque([])
+
+# a set of urls that we have already crawled for emails
 processed_urls = set()
 
 # a set of crawled emails
 emails = set()
 
-# Test 1: Print URLs from list #1
-print(new_urls)
+# process urls one by one to generate new urls until we exhaust the queue
+while len(user_urls):
 
-"""
+    # move next url from the queue to the set of processed urls
+    url = user_urls.popleft()
+    processed_user_urls.add(url)
+
+    # extract base url to resolve relative links
+    parts = urlsplit(url)
+    base_url = "{0.scheme}://{0.netloc}".format(parts)
+    path = url[:url.rfind('/')+1] if '/' in parts.path else url
+
+    # get url's content
+    print("Processing %s" % url)
+    try:
+        response = requests.get(url)
+    except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+        # ignore pages with errors
+        continue
+
     # create a beutiful soup for the html document
     soup = BeautifulSoup(response.text)
 
@@ -44,10 +66,12 @@ print(new_urls)
         elif not link.startswith('http'):
             link = path + link
         # add the new url to the queue if it was not enqueued nor processed yet and starts with the original path
-        if not link in new_urls and not link in processed_urls and user_input_contain_crawl_to_base_path in link:
+        if not link in user_urls and not link in processed_user_urls and not link in new_urls and not link in processed_urls and user_input_contain_crawl_to_base_path in link:
             new_urls.append(link)
 
+print(new_urls)
 
+"""
 # process urls one by one until we exhaust the queue
 while len(new_urls):
 
@@ -59,7 +83,6 @@ while len(new_urls):
     parts = urlsplit(url)
     base_url = "{0.scheme}://{0.netloc}".format(parts)
     path = url[:url.rfind('/')+1] if '/' in parts.path else url
-    print(path)
 
     # get url's content
     print("Processing %s" % url)
